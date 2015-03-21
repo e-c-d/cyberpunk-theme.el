de7d9da20dd09709b325fbcede753e53f25ba431
Eduard Christian Dumitrescu <edumi042@uottawa.ca>
2015-03-21 13:15:42 -0400

    Better colours for rainbow-delimiters.

--- cyberpunk-theme.el
+++ cyberpunk-theme.el
@@ -576,18 +576,15 @@
    `(outline-1 ((,class (:inherit outline-2 :height 1.0))))
 
    ;; rainbow-delimiters
-   `(rainbow-delimiters-depth-1-face ((,class (:foreground ,cyberpunk-red-1))))
-   `(rainbow-delimiters-depth-2-face ((,class (:foreground ,cyberpunk-green-2))))
-   `(rainbow-delimiters-depth-3-face ((,class (:foreground ,cyberpunk-pink-1))))
-   `(rainbow-delimiters-depth-4-face ((,class (:foreground ,cyberpunk-yellow))))
-   `(rainbow-delimiters-depth-5-face ((,class (:foreground ,cyberpunk-green))))
-   `(rainbow-delimiters-depth-6-face ((,class (:foreground ,cyberpunk-blue-3))))
-   `(rainbow-delimiters-depth-7-face ((,class (:foreground ,cyberpunk-orange))))
-   `(rainbow-delimiters-depth-8-face ((,class (:foreground ,cyberpunk-blue-2))))
-   `(rainbow-delimiters-depth-9-face ((,class (:foreground ,cyberpunk-gray))))
-   `(rainbow-delimiters-depth-10-face ((,class (:foreground ,cyberpunk-white))))
-   `(rainbow-delimiters-depth-11-face ((,class (:foreground ,cyberpunk-blue+1))))
-   `(rainbow-delimiters-depth-12-face ((,class (:foreground ,cyberpunk-red-4))))
+   `(rainbow-delimiters-depth-1-face ((,class (:foreground ,cyberpunk-orange))))
+   `(rainbow-delimiters-depth-2-face ((,class (:foreground ,cyberpunk-cyan))))
+   `(rainbow-delimiters-depth-3-face ((,class (:foreground ,cyberpunk-yellow))))
+   `(rainbow-delimiters-depth-4-face ((,class (:foreground ,cyberpunk-pink))))
+   `(rainbow-delimiters-depth-5-face ((,class (:foreground ,cyberpunk-orange))))
+   `(rainbow-delimiters-depth-6-face ((,class (:foreground ,cyberpunk-green))))
+   `(rainbow-delimiters-depth-7-face ((,class (:foreground ,cyberpunk-cyan))))
+   `(rainbow-delimiters-depth-8-face ((,class (:foreground ,cyberpunk-yellow))))
+   `(rainbow-delimiters-depth-9-face ((,class (:foreground ,cyberpunk-pink))))
 
    ;; rpm-mode
    `(rpm-spec-dir-face ((,class (:foreground ,cyberpunk-green))))
